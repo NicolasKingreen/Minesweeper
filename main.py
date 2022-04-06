be0a61d156210a4ace19b9b4a9477588cb77290d
Nicolas Kingreen <NicolasKingreen@gmail.com>
2022-04-06 17:16:16 +0300

    upgraded sprites and fixed right click

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ class Minesweeper:
         self.is_running = False
 
         self.can_click = True
-        self.lmb_pressed = False 
+        self.lmb_pressed = False
+        self.rmb_pressed = False
         self.click_cooldown = 100
         self.click_time = 0
 
@@ -26,11 +27,14 @@ class Minesweeper:
 
     def init_field(self):
         self.field = [[0 for _ in range(FIELD_SIZE)] for _ in range(FIELD_SIZE)]
+
+        # placing mines
         for _ in range(MINES):
             x = random.randint(0, FIELD_SIZE-1)
             y = random.randint(0, FIELD_SIZE-1)
             self.field[x][y] = 9
 
+        # counting mines around each cell
         for i in range(FIELD_SIZE):
             for j in range(FIELD_SIZE):
                 if self.field[i][j] == 9:
@@ -62,6 +66,7 @@ class Minesweeper:
             self.update_cooldowns()
             self.draw()
 
+            # winning condition
             if self.is_running and self.check_win_condition():
                 self.end_game(True)
 
@@ -81,25 +86,50 @@ class Minesweeper:
 
     def process_click(self):
         mouse_pos = pygame.mouse.get_pos()
-        mouse_cell = mouse_pos[0] // CELL_SIZE, mouse_pos[1] // CELL_SIZE
+
+        # converting mouse position to cell coordinates
+        mouse_cell_x = mouse_pos[0] // CELL_SIZE
+        mouse_cell_y = mouse_pos[1] // CELL_SIZE
+        mouse_cell = mouse_cell_x, mouse_cell_y
+
         mouse_buttons_pressed = pygame.mouse.get_pressed()
-        if any(mouse_buttons_pressed) and self.can_click and not self.lmb_pressed:
+        if any(mouse_buttons_pressed) and self.can_click \
+                and not (self.lmb_pressed or self.rmb_pressed):
             self.can_click = False
-            self.lmb_pressed = True
             self.click_time = pygame.time.get_ticks()
+
+            # if LMB pressed
             if mouse_buttons_pressed[0]:
-                if self.field[mouse_cell[0]][mouse_cell[1]] == 9:
+                self.lmb_pressed = True
+
+                # clicking on a mine
+                if self.field[mouse_cell_x][mouse_cell_y] == 9:
                     self.end_game(False)
-                elif self.field[mouse_cell[0]][mouse_cell[1]] == 0:
-                    for x, y in (self.get_empty_cells_around(mouse_cell[0], mouse_cell[1])):
+
+                # clicking on an empty cell (no digits)
+                elif self.field[mouse_cell_x][mouse_cell_y] == 0:
+                    for x, y \
+                    in (self.get_empty_cells_around(mouse_cell_x, mouse_cell_y)):
                         self.uncovered_tiles[x][y] = 0
+
+                # clicking on a digit cell
                 else:
                     self.uncovered_tiles[mouse_cell[0]][mouse_cell[1]] = 0 
-            elif mouse_buttons_pressed[2] and self.uncovered_tiles[mouse_cell[0]][mouse_cell[1]] != 0:
-                self.uncovered_tiles[mouse_cell[0]][mouse_cell[1]] = 2
 
+            # if RMB pressed, placing a flag
+            elif mouse_buttons_pressed[2] and \
+                self.uncovered_tiles[mouse_cell_x][mouse_cell_y] != 0:
+                self.rmb_pressed = True
+                if self.uncovered_tiles[mouse_cell_x][mouse_cell_y] == 1:
+                    self.uncovered_tiles[mouse_cell_x][mouse_cell_y] = 2
+                elif self.uncovered_tiles[mouse_cell_x][mouse_cell_y] == 2:
+                    self.uncovered_tiles[mouse_cell_x][mouse_cell_y] = 1
+
+        # controlling single click
         if not mouse_buttons_pressed[0] and self.lmb_pressed:
             self.lmb_pressed = False
+        elif not mouse_buttons_pressed[2] and self.rmb_pressed:
+            self.rmb_pressed = False 
 
     def update_cooldowns(self):
         current_time = pygame.time.get_ticks()
@@ -130,8 +160,11 @@ class Minesweeper:
     def check_win_condition(self):
         for i in range(FIELD_SIZE):
             for j in range(FIELD_SIZE):
+
+                # if there is a covered mine
                 if self.field[i][j] != 9 and self.uncovered_tiles[i][j] != 0:
                     return False
+
         return True
 
     def end_game(self, has_won):
@@ -148,18 +181,17 @@ class Minesweeper:
         self.display_surface.fill("white")
         for x, row in enumerate(self.field):
             for y, cell in enumerate(row):
+                tile_x = 0
                 if self.uncovered_tiles[x][y] == 0:
-                    self.display_surface.blit(self.sprite, 
-                                              (x * CELL_SIZE, y * CELL_SIZE), 
-                                              (CELL_SIZE * cell, 0, CELL_SIZE, CELL_SIZE))
-                elif self.uncovered_tiles[x][y] == 2:
-                    self.display_surface.blit(self.sprite,
-                                              (x * CELL_SIZE, y * CELL_SIZE),
-                                              (CELL_SIZE * 11, 0, CELL_SIZE, CELL_SIZE))
+                    tile_x = cell
+                elif self.uncovered_tiles[x][y] == 1:
+                    tile_x = 10
                 else:
-                    self.display_surface.blit(self.sprite,
-                                              (x * CELL_SIZE, y * CELL_SIZE),
-                                              (CELL_SIZE * 10, 0, CELL_SIZE, CELL_SIZE))
+                    tile_x = 11
+                self.display_surface.\
+                    blit(self.sprite,
+                        (x * CELL_SIZE, y * CELL_SIZE),
+                        (CELL_SIZE * tile_x, 0, CELL_SIZE, CELL_SIZE))
         pygame.display.update()
 
 
